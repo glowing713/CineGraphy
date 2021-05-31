68cbb3df2ccdffba3634fbd43d0922f53dcc8fd7
glowing713 <glowing713@naver.com>
2021-06-01 02:26:10 +0900

    chore: 동적으로 컴포넌트를 생성하고 prop-type 적용해봄

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ best }) {
-  return <h1>I like {best}</h1>;
+const favoriteFoods = [
+  { id: 1, name: "pizza", rating: 4.2 },
+  { id: 2, name: "ramen", rating: 3.9 },
+  { id: 3, name: "hamburger", rating: 4.4 },
+  { id: 4, name: "chicken", rating: 4.1 },
+  { id: 5, name: "french fries", rating: 5.0 },
+];
+
+// props에서 name만 사용함
+function Food({ name, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+    </div>
+  );
 }
+
+// prop의 type을 정의해서 올바르게 전달받았는지를 체크할 수 있다.
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <Food best="pizza" />
-      <Food best="ramen" />
-      <Food best="hamburger" />
-      <Food best="chicken" />
-      <Food best="french fries" />
+      {favoriteFoods.map((food) => (
+        <Food key={food.id} name={food.name} rating={food.rating} />
+      ))}
     </div>
   );
 }
