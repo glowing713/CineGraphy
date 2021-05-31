362e0ec074d5ec1e76649253a399734372adb47c
glowing713 <glowing713@naver.com>
2021-06-01 03:23:31 +0900

    chore: class component와 state 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const favoriteFoods = [
-  { id: 1, name: "pizza", rating: 4.2 },
-  { id: 2, name: "ramen", rating: 3.9 },
-  { id: 3, name: "hamburger", rating: 4.4 },
-  { id: 4, name: "chicken", rating: 4.1 },
-  { id: 5, name: "french fries", rating: 5.0 },
-];
-
-// props에서 name만 사용함
-function Food({ name, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-    </div>
-  );
-}
-
-// prop의 type을 정의해서 올바르게 전달받았는지를 체크할 수 있다.
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-function App() {
-  return (
-    <div>
-      {favoriteFoods.map((food) => (
-        <Food key={food.id} name={food.name} rating={food.rating} />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => {
+      return { count: current.count + 1 };
+    });
+  };
+  minus = () => {
+    this.setState((current) => {
+      return { count: current.count - 1 };
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}!</h1>
+        <button onClick={this.add}>Plus</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
